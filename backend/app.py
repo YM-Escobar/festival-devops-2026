7bbed9b2df5b547377fa359f59051df22819a385
Yeiner Stainer Moreno Escobar <ymescobar08@gmail.com>
2026-06-12 08:03:17 -0500

    Se agrega API Flask

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return {
+        "festival": "Pacific DevOps Music Fest",
+        "fecha": "15 de diciembre de 2026",
+        "artistas": [
+            "Imagine Dragons",
+            "Coldplay",
+            "Foo Fighters"
+        ]
+    }
+
+app.run(host="0.0.0.0", port=5000)
